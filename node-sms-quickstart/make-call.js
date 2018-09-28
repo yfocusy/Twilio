3fefdbf588bef57a311dee8f5af5e41b230883ee
Li Yu <li.yu.unsw@gmail.com>
2018-09-29 06:07:46 +1000

    Update make-call.js

--- node-sms-quickstart/make-call.js
+++ node-sms-quickstart/make-call.js
@@ -1,5 +1,5 @@
-const accountSid = 'AC3b14e6e108ab4326721157b196489b27';
-const authToken = '135e418d1aac4904b991a937ba86e6a4';
+const accountSid = 'AC3b14e6e108ab432672157b196489b27';
+const authToken = '135e418d1aac4904b91a937ba86e6a4';
 
 const client = require('twilio')(accountSid, authToken);
 
